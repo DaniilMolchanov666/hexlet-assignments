bc7f78c6326ac7776b63a207316faa8a120e429a
Cotton Pads <cottonpads1234@yandex.ru>
2023-03-29 19:58:23 +0300

    add errors homework

--- java-oop-ru/errors/src/main/java/exercise/App.java
+++ java-oop-ru/errors/src/main/java/exercise/App.java
@@ -1,5 +1,21 @@
 package exercise;
 
-// BEGIN
+public class App {
+    public static void printSquare(Circle circle) throws NegativeRadiusException {
 
-// END
+        if (circle.getRadius() < 0) {
+            try {
+                throw new NegativeRadiusException("");
+            } catch (NegativeRadiusException e) {
+                System.out.println("Не удалось посчитать площадь\nВычисление окончено");
+            }
+        } else {
+                System.out.println(circle.getSquare() + "\nВычисление окончено");
+            }
+        }
+
+    public static void main(String[] args) throws NegativeRadiusException {
+        Circle circle = new Circle(new Point(1, 2), -1);
+        App.printSquare(circle);
+    }
+}
--- java-oop-ru/errors/src/main/java/exercise/Circle.java
+++ java-oop-ru/errors/src/main/java/exercise/Circle.java
@@ -1,5 +1,24 @@
 package exercise;
 
-// BEGIN
+class Circle {
 
-// END
+    private final Point point;
+
+    private final int radius;
+
+    public Circle(Point point, int radius) {
+        this.point = point;
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public int getSquare() throws NegativeRadiusException {
+        if (!(radius < 0)) {
+            return (int) Math.round(Math.PI * radius * radius);
+        }
+        throw new NegativeRadiusException("");
+    }
+}
