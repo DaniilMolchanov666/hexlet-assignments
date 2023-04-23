46b1cc464b230e50dd93cf5bcbfd95d6a800d2af
DaniilMolchanov666 <danya.molch@yandex.ru>
2023-04-23 18:55:19 +0300

    add all

---
+++ java-web-ru/deploy/src/main/java/exercise/App.java
@@ -0,0 +1,44 @@
+package exercise;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.Context;
+import java.io.File;
+
+import exercise.servlet.WelcomeServlet;
+import exercise.servlet.CompaniesServlet;
+
+public class App {
+
+    private static int getPort() {
+        String port = System.getenv("PORT");
+        if (port != null) {
+            return Integer.valueOf(port);
+        }
+        return 8000;
+    }
+
+    public static Tomcat getApp(int port) {
+        Tomcat app = new Tomcat();
+
+        app.setBaseDir(System.getProperty("java.io.tmpdir"));
+        app.setPort(port);
+
+        Context ctx = app.addContext("", new File(".").getAbsolutePath());
+
+        app.addServlet(ctx, WelcomeServlet.class.getSimpleName(), new WelcomeServlet());
+        ctx.addServletMappingDecoded("", WelcomeServlet.class.getSimpleName());
+
+        // BEGIN
+        
+        // END
+
+        return app;
+    }
+
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat app = getApp(getPort());
+        app.start();
+        app.getServer().await();
+    }
+}
---
+++ java-web-ru/deploy/src/main/java/exercise/servlet/CompaniesServlet.java
@@ -0,0 +1,25 @@
+package exercise.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import static exercise.Data.getCompanies;
+
+public class CompaniesServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+                throws IOException, ServletException {
+
+        // BEGIN
+        
+        // END
+    }
+}
