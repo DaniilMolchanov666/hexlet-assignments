d6fd8a1bd2d5655dd1f951a1040242c79632c980
DaniilMolchanov666 <cottonpads1234@yandex.ru>
2023-04-29 18:35:32 +0300

    add servletes logic

--- java-web-ru/deploy/src/main/java/exercise/servlet/CompaniesServlet.java
+++ java-web-ru/deploy/src/main/java/exercise/servlet/CompaniesServlet.java
@@ -1,5 +1,7 @@
 package exercise.servlet;
 
+import exercise.Data;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import static exercise.Data.getCompanies;
 
@@ -18,8 +21,25 @@ public class CompaniesServlet extends HttpServlet {
                       HttpServletResponse response)
                 throws IOException, ServletException {
 
-        // BEGIN
-        
-        // END
+        PrintWriter p = response.getWriter();
+
+        List<String> listOfCompanies = Data.getCompanies();
+
+        if (!(request.getQueryString() == null)
+                 && request.getQueryString().contains("search")) {
+
+            String s = request.getParameter("search");
+
+            String newListOfCompanies = listOfCompanies.stream()
+                    .filter(string -> string.contains(s))
+                    .collect(Collectors.joining("\n"))
+                    .trim();
+            String res = newListOfCompanies.length() > 0 ? newListOfCompanies: "Companies not found";
+            p.println(res.trim());
+            
+        } else {
+            p.println(String.join("\n", listOfCompanies).trim());
+        }
     }
 }
+
