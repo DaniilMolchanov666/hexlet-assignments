c43f0620a5b97b749e07e5375af57cae75f648b6
DaniilMolchanov666 <cottonpads1234@yandex.ru>
2023-05-18 21:14:52 +0300

    add servlet homework

---
+++ java-web-ru/servlet/src/main/java/exercise/App.java
@@ -0,0 +1,39 @@
+package exercise;
+
+import exercise.servlet.WelcomeServlet;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.WebAnnotationSet;
+
+import java.io.File;
+
+public class App {
+
+    private static int getPort() {
+        String port = System.getenv("PORT");
+        if (port != null) {
+            return Integer.valueOf(port);
+        }
+        return 8000;
+    }
+
+    public static Tomcat getApp(int port) {
+        Tomcat app = new Tomcat();
+
+        app.setBaseDir(System.getProperty("java.io.tmpdir"));
+        app.setPort(port);
+
+        Context ctx = app.addContext("", new File(".").getAbsolutePath());
+        app.addServlet(ctx, WelcomeServlet.class.getSimpleName(), new WelcomeServlet());
+        ctx.addServletMappingDecoded("", WelcomeServlet.class.getSimpleName());
+
+        return app;
+    }
+
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat app = getApp(getPort());
+        app.start();
+        app.getServer().await();
+    }
+}
